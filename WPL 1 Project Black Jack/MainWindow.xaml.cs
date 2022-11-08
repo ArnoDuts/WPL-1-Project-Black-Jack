d51d87ad611b6deb3b38cd60d3659bf5b6893b95
ArnoDuts <arnodutsofficial@gmail.com>
2022-11-08 16:00:45 +0100

    Deel Button Gecodeerd

--- WPL 1 Project Black Jack/MainWindow.xaml.cs
+++ WPL 1 Project Black Jack/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +25,79 @@ namespace WPL_1_Project_Black_Jack
         {
             InitializeComponent();
         }
+
+        
+
+        private void BtnDeel_Click(object sender, RoutedEventArgs e)
+        {
+            Random deelkaarten = new Random();
+            BtnDeel.IsEnabled = false;
+            BtnHit.IsEnabled = true;
+            BtnStand.IsEnabled = true;
+            TxbResultaat.Text = "♠ Let's play Blackjack ♣";
+            int aantalkaartenspeler = 2;
+            int aantalkaartenbank = 1;
+            List<string> kaarten = new List<string>()
+            {
+            "Aas", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Dame", "koning"
+            };
+            
+            string soort = null;
+            int kaartsoorten = 1;
+
+
+            for (int i = 0; i < aantalkaartenspeler; i++)
+            {
+                kaartsoorten = deelkaarten.Next(1, 4);
+
+                switch (kaartsoorten)
+                {
+                    case 1:
+                        soort = "Harten";
+                        break;
+                    case 2:
+                        soort = "Ruiten";
+                        break;
+                    case 3:
+                        soort = "Schoppen";
+                        break;
+                    case 4:
+                        soort = "Klaveren";
+                        break;
+
+                }
+                
+                int index = deelkaarten.Next(kaarten.Count);
+                TbxKaartenSpeler.Text += soort + " " + kaarten[index] + "\r" ;
+               
+            }
+            for (int i = 0; i < aantalkaartenbank; i++)
+            {
+                kaartsoorten = deelkaarten.Next(1, 4);
+
+                switch (kaartsoorten)
+                {
+                    case 1:
+                        soort = "Harten";
+                        break;
+                    case 2:
+                        soort = "Ruiten";
+                        break;
+                    case 3:
+                        soort = "Schoppen";
+                        break;
+                    case 4:
+                        soort = "Klaveren";
+                        break;
+
+                }
+
+                int index = deelkaarten.Next(kaarten.Count);
+                TbxKaartenBank.Text = soort + " " + kaarten[index];
+            }
+            
+        }
+
+        
     }
 }
