9b8bd6ab9fc5e1c604167b8bf13394201ce74f58
ArnoDuts <arnodutsofficial@gmail.com>
2022-12-12 17:07:09 +0100

    Deel knop Opgeschoond + Hit en Stand Knop toegevoegd

--- WPL 1 Project Black Jack/MainWindow.xaml.cs
+++ WPL 1 Project Black Jack/MainWindow.xaml.cs
@@ -26,79 +26,141 @@ namespace WPL_1_Project_Black_Jack
             InitializeComponent();
         }
 
+        Random deelkaarten = new Random();
+        int AantalKaarten = 2;
+        readonly List<string> kaarten = new List<string>()
+            {
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Dame", "Koning"
+            };
+        string soort = null;
+        int kaartsoorten = 1;
+        List<int> totaalSpeler = new List<int>();
+        List<int> totaalBank = new List<int>();
+        public string GeefKaart(Boolean isSpeler)
+        {
+            kaartsoorten = deelkaarten.Next(1, 4);
+
+            switch (kaartsoorten)
+            {
+                case 1:
+                    soort = "Harten";
+                    break;
+                case 2:
+                    soort = "Ruiten";
+                    break;
+                case 3:
+                    soort = "Schoppen";
+                    break;
+                case 4:
+                    soort = "Klaveren";
+                    break;
+
+            }
+            int index = deelkaarten.Next(kaarten.Count);
+            if (isSpeler == true)
+            {
+                if (kaarten[index] == "Boer" || kaarten[index] == "Dame" || kaarten[index] == "Koning")
+                {
+                    totaalSpeler.Add(10);
+                }
+                else
+                {
+                    totaalSpeler.Add(Convert.ToInt32(kaarten[index]));
+                }
+                TxbTotaalSpeler.Text = Convert.ToString(totaalSpeler.Sum());
+            }
+            if (isSpeler == false)
+            {
+                if (kaarten[index] == "Boer" || kaarten[index] == "Dame" || kaarten[index] == "Koning")
+                {
+                    totaalBank.Add(10);
+                }
+                else
+                {
+                    totaalBank.Add(Convert.ToInt32(kaarten[index]));
+                }
+                TxbTotaalBank.Text = Convert.ToString(totaalBank.Sum());
+            }
+            
+            return soort + " " + kaarten[index] + "\r";
+        }
+
         
 
         private void BtnDeel_Click(object sender, RoutedEventArgs e)
         {
-            Random deelkaarten = new Random();
+          
             BtnDeel.IsEnabled = false;
             BtnHit.IsEnabled = true;
             BtnStand.IsEnabled = true;
             TxbResultaat.Text = "♠ Let's play Blackjack ♣";
-            int aantalkaartenspeler = 2;
-            int aantalkaartenbank = 1;
-            List<string> kaarten = new List<string>()
-            {
-            "Aas", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Boer", "Dame", "koning"
-            };
+            TbxKaartenSpeler.Text = "";
+            TbxKaartenBank.Text = "";
+
             
-            string soort = null;
-            int kaartsoorten = 1;
+            
 
 
-            for (int i = 0; i < aantalkaartenspeler; i++)
+            for (int i = 0; i < AantalKaarten; i++)
             {
-                kaartsoorten = deelkaarten.Next(1, 4);
-
-                switch (kaartsoorten)
-                {
-                    case 1:
-                        soort = "Harten";
-                        break;
-                    case 2:
-                        soort = "Ruiten";
-                        break;
-                    case 3:
-                        soort = "Schoppen";
-                        break;
-                    case 4:
-                        soort = "Klaveren";
-                        break;
-
-                }
                 
-                int index = deelkaarten.Next(kaarten.Count);
-                TbxKaartenSpeler.Text += soort + " " + kaarten[index] + "\r" ;
-               
+                TbxKaartenSpeler.Text += GeefKaart(true);
             }
-            for (int i = 0; i < aantalkaartenbank; i++)
+            
+            AantalKaarten = 1;
+            for (int i = 0; i < AantalKaarten; i++)
             {
-                kaartsoorten = deelkaarten.Next(1, 4);
 
-                switch (kaartsoorten)
-                {
-                    case 1:
-                        soort = "Harten";
-                        break;
-                    case 2:
-                        soort = "Ruiten";
-                        break;
-                    case 3:
-                        soort = "Schoppen";
-                        break;
-                    case 4:
-                        soort = "Klaveren";
-                        break;
-
-                }
-
-                int index = deelkaarten.Next(kaarten.Count);
-                TbxKaartenBank.Text = soort + " " + kaarten[index];
+                TbxKaartenBank.Text += GeefKaart(false);
             }
             
 
         }
 
-        
+        private void BtnHit_Click(object sender, RoutedEventArgs e)
+        {
+            TbxKaartenSpeler.Text+= GeefKaart(true);
+            if (Convert.ToInt32(TxbTotaalSpeler.Text) > 21)
+            {
+                TxbResultaat.Text = "Verloren";
+                TxbResultaat.Foreground = Brushes.Red;
+            }
+            if(Convert.ToInt32(TxbTotaalBank.Text) > 21)
+            {
+                TxbResultaat.Text = "Gewonnen";
+                TxbResultaat.Foreground = Brushes.Green;
+            }
+            if (Convert.ToInt32(TxbTotaalSpeler.Text) == 21)
+            {
+                TxbResultaat.Text = "Blackjack!";
+                TxbResultaat.Foreground = Brushes.Green;
+            }
+            
+        }
+
+        private void BtnStand_Click(object sender, RoutedEventArgs e)
+        {
+            TbxKaartenBank.Text += GeefKaart(false);
+
+            if (Convert.ToInt32(TxbTotaalBank.Text) >= 17)
+            {
+                if(Convert.ToInt32(TxbTotaalBank.Text) > 21)
+                {
+                    TxbResultaat.Text = "Gewonnen";
+                    TxbResultaat.Foreground = Brushes.Green;
+                }
+                if(Convert.ToInt32(TxbTotaalBank.Text) < Convert.ToInt32(TxbTotaalSpeler.Text))
+                {
+                    TxbResultaat.Text = "Gewonnen";
+                    TxbResultaat.Foreground = Brushes.Green;
+                }
+                else
+                {
+                    TxbResultaat.Text = "Verloren";
+                    TxbResultaat.Foreground = Brushes.Red;
+                }
+            }
+            
+        }
     }
 }
